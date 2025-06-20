3ae92b9d766e138e5b0067266269a0af547b8da7
241019 <241019@std.hi-joho.ac.jp>
2025-06-20 12:02:21 +0900

    迫真オブジェクト指向部　javaの裏技

--- src/Character.java
+++ src/Character.java
@@ -1,7 +1,6 @@
 public abstract class Character implements Creature{
     String name;
     int hp;
-    public Character(String name, int hp) {}
     public abstract void attack(Character target);
 
     public final boolean isAlive() {
