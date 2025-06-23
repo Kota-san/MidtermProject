0a40041e5a7af8aaa257ad9b9bae8326463d4252
241019 <241019@std.hi-joho.ac.jp>
2025-06-23 11:16:05 +0900

    迫真オブジェクト指向部　javaの裏技

--- src/Slime.java
+++ src/Slime.java
@@ -2,7 +2,6 @@ public final class Slime extends Character{
     public Slime(String name, int hp){
         super(name, hp);
     }
-    @Override
     public void attack(Character target) {
         System.out.println(this.name + "は体当たりで攻撃！" + target.name + "に5のダメージを与えた！」");
         target.hp = target.hp - 5;
